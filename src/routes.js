ca171338a30fa368aa0551f9d7f35eafb9981303
Natália Souza <46005006+nathtor4@users.noreply.github.com>
2023-07-03 13:13:23 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,26 @@ import CadastroCliente from "pages/CadastroCliente";
 import PaginaBase from "pages/PaginaBase";
 import PaginaNaoEncontrada from "./pages/PaginaNaoEncontrada";
 import EditarCliente from "pages/EditarCliente";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const { BrowserRouter, Route, Routes } = require("react-router-dom");
 
 function AppRoutes() {
     return (
         <BrowserRouter>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
             <Routes>
                 <Route path="/" element={<PaginaBase />}>
                     <Route path="clientes" element={<CadastroCliente />} />
