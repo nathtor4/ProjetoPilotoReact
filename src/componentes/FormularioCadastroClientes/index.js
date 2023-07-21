b73c0ef668df7d67f510be5f8969da68c4e51ab3
André Topázio <andre.topazio@gmail.com>
2023-07-21 09:08:00 -0300

    adicionado um tratamento para apresentar a mensagem que vem da API

--- src/componentes/FormularioCadastroClientes/index.js
+++ src/componentes/FormularioCadastroClientes/index.js
@@ -2,11 +2,11 @@ import CampoFormulario from "componentes/CampoFormulario";
 import CampoFormularioCheckbox from "componentes/CampoFormularioCheckbox";
 import "./FormularioCadastroClientes.css";
 
-import {useForm} from "react-hook-form";
-import {yupResolver} from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
-import {CPFValido} from "componentes/ValidaCPF";
+import { CPFValido } from "componentes/ValidaCPF";
 
 import InputMask from 'react-input-mask';
 import axios from "axios";
@@ -19,29 +19,30 @@ export default function FormularioCadastroClientes() {
         telefone: yup.string().required("Campo obrigatório").min(11, "Telefone inválido").max(15, "Telefone inválido").matches(/\(?[0-9]{2}\)?[\s][0-9]{5}-?[0-9]{4}/, "Formato inválido"),
         email: yup.string().required("Campo obrigatório").email("E-mail inválido"),
         cpf: yup.string().required("Campo obrigatório").min(11, "CPF inválido")
-        .max(14, "CPF inválido")
-        .test('validacaoCPF', 'CPF inválido', CPFValido),
+            .max(14, "CPF inválido")
+            .test('validacaoCPF', 'CPF inválido', CPFValido),
         checkboxes: yup.array().required('Selecione pelo menos uma opção').min(1, 'Selecione pelo menos uma opção').transform((value) => (Array.isArray(value) ? value : [])),
     })
 
-    const {handleSubmit, register, formState: { errors }, reset, setValue} = useForm({
+    const { handleSubmit, register, formState: { errors }, reset, setValue } = useForm({
         resolver: yupResolver(schemaDeValidacao)
     })
 
 
     const cadastro = async (variaveis) => {
 
-        const dados = { 
-            "nome": variaveis.nome, 
-            "cpf": variaveis.cpf, 
-            "email": variaveis.email, 
-            "telefone": variaveis.telefone}
-        
+        const dados = {
+            "nome": variaveis.nome,
+            "cpf": variaveis.cpf,
+            "email": variaveis.email,
+            "telefone": variaveis.telefone
+        }
+
         console.log(dados);
 
-        try{
+        try {
             await axios.post('https://contratos.smedtecnologia.com.br/contratos-service/estag-clientes', dados);
-            toast.success("Cadastrado com sucesso!", 
+            toast.success("Cadastrado com sucesso!",
                 {
                     position: "top-right",
                     autoClose: 1000,
@@ -53,20 +54,39 @@ export default function FormularioCadastroClientes() {
                     theme: "light",
                 }
             );
-        } catch(error) {
+        } catch (error) {
+           
             console.log(error);
-            toast.error("Ocorreu algum erro com a API!",
-                {
-                    position: "top-right",
-                    autoClose: 1000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "light",
-                }
-            );
+
+            const mensagem = error.response?.data?.message;
+            if (mensagem) {
+                toast.error(mensagem,
+                    {
+                        position: "top-right",
+                        autoClose: 1000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "light",
+                    }
+                );
+            } else {
+                toast.error("Ocorreu um erro no API",
+                    {
+                        position: "top-right",
+                        autoClose: 1000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "light",
+                    }
+                );
+            }
+
         }
     }
 
@@ -81,76 +101,76 @@ export default function FormularioCadastroClientes() {
 
         reset({
             nome: '',
-            telefone:'',
+            telefone: '',
             email: '',
             cpf: '',
             checkboxes: [],
         })
     }
-    
-    return(
+
+    return (
         <form className="m-5" data-formulario onSubmit={handleSubmit(save)} >
-            
-            <CampoFormulario 
+
+            <CampoFormulario
                 tituloCampo="Nome:"
                 id="name"
-                required = "required"
-                erro = {errors?.nome && <p className="mensagem-erro">{errors?.nome?.message}</p>  }
-                tagInput= {<input 
-                                type="text" 
-                                id="name" 
-                                className={`form-control ${errors.nome ? "erro": " "}`}
-                                {...register("nome")} 
-                                autoComplete="off"
-                            >
-                            </input>}
-            />    
-
-            <CampoFormulario 
-                tituloCampo="Telefone:"
-                id="tel"
-                required = "required"
-                erro = {errors?.telefone && <p className="mensagem-erro">{errors?.telefone?.message}</p>  }
-                tagInput= {<InputMask 
-                                type="tel" 
-                                id="tel" 
-                                className={`form-control ${errors.telefone ? "erro": " "}`}  
-                                {...register("telefone")}
-                                autoComplete="off"
-                                mask={"(99) 99999-9999"}
-                            >
-                            </InputMask>}
-            />        
-
-            <CampoFormulario 
-                tituloCampo="CPF:"
-                id="CPF"
-                required = "required"
-                erro = {errors?.cpf && <p className="mensagem-erro">{errors?.cpf?.message}</p>  }
-                tagInput= {<InputMask 
-                                type="text" 
-                                id="CPF" 
-                                className={`form-control ${errors.cpf ? "erro": " "}`}  
-                                {...register("cpf")} 
-                                autoComplete="off"
-                                mask={"999.999.999-99"}
-                            >
-                            </InputMask>}
+                required="required"
+                erro={errors?.nome && <p className="mensagem-erro">{errors?.nome?.message}</p>}
+                tagInput={<input
+                    type="text"
+                    id="name"
+                    className={`form-control ${errors.nome ? "erro" : " "}`}
+                    {...register("nome")}
+                    autoComplete="off"
+                >
+                </input>}
             />
 
-            <CampoFormulario 
+            <CampoFormulario
+                tituloCampo="Telefone:"
+                id="tel"
+                required="required"
+                erro={errors?.telefone && <p className="mensagem-erro">{errors?.telefone?.message}</p>}
+                tagInput={<InputMask
+                    type="tel"
+                    id="tel"
+                    className={`form-control ${errors.telefone ? "erro" : " "}`}
+                    {...register("telefone")}
+                    autoComplete="off"
+                    mask={"(99) 99999-9999"}
+                >
+                </InputMask>}
+            />
+
+            <CampoFormulario
+                tituloCampo="CPF:"
+                id="CPF"
+                required="required"
+                erro={errors?.cpf && <p className="mensagem-erro">{errors?.cpf?.message}</p>}
+                tagInput={<InputMask
+                    type="text"
+                    id="CPF"
+                    className={`form-control ${errors.cpf ? "erro" : " "}`}
+                    {...register("cpf")}
+                    autoComplete="off"
+                    mask={"999.999.999-99"}
+                >
+                </InputMask>}
+            />
+
+            <CampoFormulario
                 tituloCampo="Email:"
-                id="email" 
-                required = "required"
-                erro = {errors?.email && <p className="mensagem-erro">{errors?.email?.message}</p>  }
-                tagInput= {<input 
-                                type="email" 
-                                id="email" 
-                                className={`form-control ${errors.email ? "erro": " "}`}  
-                                {...register("email")} 
-                                autoComplete="off"
-                            >
-                            </input>}        
+                id="email"
+                required="required"
+                erro={errors?.email && <p className="mensagem-erro">{errors?.email?.message}</p>}
+                tagInput={<input
+                    type="email"
+                    id="email"
+                    className={`form-control ${errors.email ? "erro" : " "}`}
+                    {...register("email")}
+                    autoComplete="off"
+                >
+                </input>}
             />
 
             <div className="row mb-3">
@@ -160,67 +180,67 @@ export default function FormularioCadastroClientes() {
                 </div>
                 <div className="col-sm-10 offset-sm-2">
                     <div>
-                        <CampoFormularioCheckbox 
+                        <CampoFormularioCheckbox
                             id="inlineCheckbox1"
                             nomeCampo="Artes"
-                            tagInput={<input 
-                                        className={`form-check-input border-2 checkbox ${errors.checkboxes ? "erro": " "}`}  
-                                        type="checkbox" 
-                                        id="inlineCheckbox1"
-                                        value="Artes"
-                                        {...register("checkboxes")}
-                                       >
-                                       </input>}
+                            tagInput={<input
+                                className={`form-check-input border-2 checkbox ${errors.checkboxes ? "erro" : " "}`}
+                                type="checkbox"
+                                id="inlineCheckbox1"
+                                value="Artes"
+                                {...register("checkboxes")}
+                            >
+                            </input>}
                         />
-                        <CampoFormularioCheckbox 
+                        <CampoFormularioCheckbox
                             id="inlineCheckbox2"
                             nomeCampo="Esportes"
-                            tagInput={<input 
-                                        className={`form-check-input border-2 checkbox ${errors.checkboxes ? "erro": " "}`}
-                                        type="checkbox" 
-                                        id="inlineCheckbox2"
-                                        value="Esportes"
-                                        {...register("checkboxes")}
-                                      >
-                                      </input>}
+                            tagInput={<input
+                                className={`form-check-input border-2 checkbox ${errors.checkboxes ? "erro" : " "}`}
+                                type="checkbox"
+                                id="inlineCheckbox2"
+                                value="Esportes"
+                                {...register("checkboxes")}
+                            >
+                            </input>}
                         />
-                        <CampoFormularioCheckbox 
+                        <CampoFormularioCheckbox
                             id="inlineCheckbox3"
                             nomeCampo="Dança"
-                            tagInput={<input 
-                                        className={`form-check-input border-2 checkbox ${errors.checkboxes ? "erro": " "}`} 
-                                        type="checkbox" 
-                                        id="inlineCheckbox3"
-                                        value="Dança"
-                                        {...register("checkboxes")}
-                                      >
-                                      </input>}
-                            
+                            tagInput={<input
+                                className={`form-check-input border-2 checkbox ${errors.checkboxes ? "erro" : " "}`}
+                                type="checkbox"
+                                id="inlineCheckbox3"
+                                value="Dança"
+                                {...register("checkboxes")}
+                            >
+                            </input>}
+
                         />
-                        <CampoFormularioCheckbox 
+                        <CampoFormularioCheckbox
                             id="inlineCheckbox4"
                             nomeCampo="Ficção"
-                            tagInput={<input 
-                                        className={`form-check-input border-2 checkbox ${errors.checkboxes ? "erro": " "}`}
-                                        type="checkbox" 
-                                        id="inlineCheckbox4"
-                                        value="Ficção"
-                                        {...register("checkboxes")}
-                                      >
-                                      </input>}
+                            tagInput={<input
+                                className={`form-check-input border-2 checkbox ${errors.checkboxes ? "erro" : " "}`}
+                                type="checkbox"
+                                id="inlineCheckbox4"
+                                value="Ficção"
+                                {...register("checkboxes")}
+                            >
+                            </input>}
                         />
                     </div>
                 </div>
             </div>
-                
+
             <div className="d-flex justify-content-md-end m-1">
-            <button 
-                type="submit" 
-                className="btn btn-primary botao button-verificador"
-            >
-                Salvar
-            </button> 
+                <button
+                    type="submit"
+                    className="btn btn-primary botao button-verificador"
+                >
+                    Salvar
+                </button>
             </div>
-        </form>                      
+        </form>
     )
 }
